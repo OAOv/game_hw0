6070ccd4d76022132d25706965dfbc517df6ca28
OAOv <leo877.OAO@gmail.com>
2018-12-28 13:40:23 +0800

    update speed

--- hw6/agent0.js
+++ hw6/agent0.js
@@ -25,7 +25,7 @@ class Agent {
     this.target = null;
     this.halfSize = halfSize;  // half width
     this.mesh = agentMesh (this.halfSize, 'cyan');
-    this.MAXSPEED = 500;
+    this.MAXSPEED = 300;
     this.ARRIVAL_R = 30;
     
     this.score = 0;
@@ -64,8 +64,8 @@ class Agent {
       let vhat = this.vel.clone().normalize();
       let point = obs[i].center.clone().sub (this.pos) // c-p
       let proj  = point.dot(vhat);
-      const REACH = 80
-      const K = 150
+      const REACH = 60
+      const K = 75
 
       if (proj >= 0 && proj <= REACH && proj < this.minProj) {
         let perp = new THREE.Vector3();
